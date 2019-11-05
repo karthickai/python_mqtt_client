85c44db71110de8270188f666ea78b1ef3f5f245
karthickai <karthickps.ai@gmail.com>
2019-11-06 01:17:16 +0530

    file removed

--- mqtt.py
+++ mqtt.py
@@ -21,17 +21,13 @@ def on_log(mqttc, obj, level, string):
     print(string)
 
 
-# If you want to use a specific client id, use
-# mqttc = mqtt.Client("client-id")
-# but note that the client id must be unique on the broker. Leaving the client
-# id parameter empty will generate a random id for you.
+
 mqttc = mqtt.Client()
 mqttc.on_message = on_message
 mqttc.on_connect = on_connect
 mqttc.on_publish = on_publish
 mqttc.on_subscribe = on_subscribe
-# Uncomment to enable debug messages
-# mqttc.on_log = on_log
+mqttc.on_log = on_log
 mqttc.connect("127.0.0.1", 1883, 60)
 mqttc.subscribe("$SYS/#", 0)
 
